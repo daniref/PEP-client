bf1790e082707a9804e4d24fcb759b79c2689afa
Daniele Lombardi <daniele.lombardi4@unina.it>
2025-07-03 11:29:05 +0200

    doc: update HandlerResults doc

--- core/Handlers/HandlerResults.py
+++ core/Handlers/HandlerResults.py
@@ -4,10 +4,18 @@ from core.Utility import *
 from core.Metrics import Parser as PS
 from core.Metrics import Metrics as MT
 
-oscillPeriod = 0.01 # It represents the oscillation period in which the ro-PUF works
+# oscillPeriod represents the oscillation period in which the ro-PUF works. 
+# It is used to calculate the frequencies of the counter1 and counter2 values in the CRP files.
+oscillPeriod = 0.01 
 
 def ExpCampaignDownloading(idTest,username,password):
-    
+    """
+    Download the experiment campaigns for a given test ID and save them as CSV files.
+    Args:
+        idTest (int): The ID of the test for which to download the campaigns.
+        username (str): The username of the user.
+        password (str): The password of the user.
+    """
     idCampaignList = _ReadIdCampaigns(idTest)
 
     for idCampaign in idCampaignList:
@@ -15,9 +23,9 @@ def ExpCampaignDownloading(idTest,username,password):
         data = {'idCampaign': idCampaign,'username': username, 'password': password}
         response = requests.post(httpUrl, data=data, verify=certFile)
 
-        # Verifica se la richiesta è andata a buon fine (codice di stato 200)
+        # Check if the response is successful
         if response.status_code == 200:
-            # Presupponiamo che la risposta sia in formato JSON e contenga i campi 'csv', 'field' e 'line'
+            # we assume the response is in JSON format and contains the fields 'csv', 'field', and 'line'
             try:
                 responseData = response.json()
                 csvData = responseData['csv']
@@ -33,7 +41,7 @@ def ExpCampaignDownloading(idTest,username,password):
 
             filePath = f"{resultsDirectory}/expCampaign_{idCampaign}.csv"
 
-            # Salva il CSV nel file
+            # Save the CSV data to a file
             try:
                 with open(filePath, mode='w', newline='') as csvFile:
                     writer = csv.writer(csvFile, delimiter=fieldDelimiter, lineterminator=lineDelimiter)
@@ -43,69 +51,74 @@ def ExpCampaignDownloading(idTest,username,password):
             except IOError as e:
                 print(f"Errore durante il salvataggio del file per idCampaign {idCampaign}: {e}")
         else:
-            # Se la richiesta non è andata a buon fine, gestisci l'errore
+            # Log the error with the status code
             print(f"[CLIENT-APP] - Request Exp Campaign failed for idCampaign {idCampaign} with code {response.status_code}")
 
-def GetInfoExps(idCampaign,username,password):
-    httpUrl = urljoin(GetServerAddress(), httpInfoExps)
-    data={'idcampaign': idCampaign,'username': username, 'password': password}
-    response = requests.post(httpUrl,data=data, verify=certFile)
-
-    # Verifica se la richiesta è andata a buon fine (codice di stato 200)
-    if response.status_code == 200:
-        print('[CLIENT-APP] - GetInfoExps() - Obtained Info about Exp Campaign ',idCampaign)
-        return response.json()['num_exps']
-    else:
-        # Se la richiesta non è andata a buon fine, gestisci l'errore
-        print('[CLIENT-APP] - GetInfoExps() - Request for info about Exp failed ', response.status_code)
-
 def _ReadIdCampaigns(idTest):
+    """
+    Reads the idCampaigns from the campaigns.csv file for a given test ID.
+    Args:
+        idTest (int): The ID of the test for which to read the campaigns.
+    Returns:
+        list: A list of idCampaigns.
+    """
     testDir = baseTestsDir+str(idTest)+'/'
-    # Path del file csv
     filePath = os.path.join(testDir, 'campaigns.csv')
     
-    # Verifica se il file esiste
+    # Check if the file exists
     if not os.path.isfile(filePath):
         print(f"Il file {filePath} non esiste.")
         return []
     
-    # Leggi il file csv utilizzando pandas
+    # Read the CSV file using pandas
     try:
         df = pd.read_csv(filePath)
-        # Restituisci la lista degli idCampaign
+        # Return a list of idCampaigns
         return df['idCampaign'].tolist()
     except KeyError:
-        print(f"La colonna 'idCampaign' non esiste nel file.")
+        print(f"The column 'idCampaign' does not exist in the file.")
         return []
 
 def CheckCRPsExist(idTest):
     """
-    Checks if CRP (Campaign Result Package) files exist for a given test campaign.
-    @param idTest: The ID of the test to check for CRP file existence.
-    @type idTest: int
-    @param idCampaignsList: List of campaign IDs to check for CRP file existence.
-    @type idCampaignsList: list[int]
-    @return: True if all required CRP files exist, False otherwise.
-    @rtype: bool
+    Checks if the CRP files for the given test ID exist in the results directory.
+    Args:
+        idTest (int): The ID of the test for which to check the CRP files.
+    Returns:
+        bool: True if all required CRP files exist, False otherwise.
     """
     idCampaignsList = _ReadIdCampaigns(idTest)
     resultsDir = baseTestsDir+str(idTest)+'/results'
     
-    # Verifica se il path esiste
+    # Check if the results directory exists
     if not os.path.exists(resultsDir):
         print(f"Il path {resultsDir} non esiste.")
         return False
     
-    # Set con gli ID richiesti trasformati in stringhe (per confronti diretti)
+    # Assuming idCampaignsList contains integers, convert them to strings for file naming
     filesToCheckList = {f"expCampaign_{id}.csv" for id in idCampaignsList}
     
-    # Set con i nomi dei file presenti nella directory
+    # Use set to avoid duplicates and for efficient membership testing
     filePresent = set(os.listdir(resultsDir))
     
-    # Verifica se tutti i file richiesti sono presenti
+    # Check if all files in filesToCheckList are present in filePresent
     return filesToCheckList.issubset(filePresent)
 
-def GetTestInfo(idTest):
+def _GetTestInfo(idTest):
+    """
+    Reads the test information from the campaigns.csv and expsConfig.xml files for a given test ID.
+    Args:
+        idTest (int): The ID of the test for which to read the information.
+    Returns:
+        tuple: A tuple containing:
+            - idPufLists: List of lists of PUF IDs for each experiment.
+            - numChallengeList: List of number of challenges used in each experiment.
+            - respWidthList: List of response widths for each experiment.
+            - numRepsList: List of number of repetitions for each experiment.
+            - devicesList: List of device IDs.
+            - campaignList: List of campaign IDs.
+            - hasCountersList: List of boolean values indicating if counters are used in each experiment.
+    """
     testDir = baseTestsDir + str(idTest)
 
     campaignList = [] # List of campaign ids
@@ -116,60 +129,55 @@ def GetTestInfo(idTest):
     idPufLists = [] # List of list of puf ids for each experiment (max= number of different experiments)
     hasCountersList = []  # List of boolean values for each experiment (max= number of different experiments)
 
-    # Leggi il file campaigns CSV
+    # Read the campaigns CSV file
     df = pd.read_csv(testDir + '/campaigns.csv')
 
-    # Legge le occorrenze di idDev
+    # Retrieves the unique device IDs and campaign IDs from the DataFrame
     devicesList = df['idDev'].tolist()
     campaignList = df['idCampaign'].tolist()
 
-    # Leggi le configurazioni degli esperimenti nel file expsConfig.xml
+    # Read the experiment configurations from the expsConfig.xml file
     tree = ET.parse(testDir + '/expsConfig.xml')
     root = tree.getroot()
 
-    # Leggi le informazioni sui PUF dal file pufsConfig.xml
+    # Reads the PUF configurations from the pufsConfig.xml file
     pufTree = ET.parse(testDir + '/pufsConfig.xml')
     pufRoot = pufTree.getroot()
     pufInstances = pufRoot.findall("PUFInstance")
 
-    # Itera su ogni elemento figlio di root e gestisce in base al tipo di esperimento
+    # Cycle through each experiment element in the XML
     for expElement in root:
-        expType = expElement.tag  # Ottieni il tipo di esperimento (es. "RangeExp", "ListExp", "RandomExp")
-
+        expType = expElement.tag  # Get the type of experiment (e.g., RangeExp, ListExp, RandomExp)
         numExps = expElement.find("num_exps")
-        numRepsList.append(int(numExps.text))  # Converte in intero e aggiunge alla lista
-
-        if expType == "RangeExp":
-            # Gestisce l'esperimento di tipo RangeExp
+        numRepsList.append(int(numExps.text))  # Convert the number of experiments to an integer and add it to the list
+        if expType == "RangeExp": # Handles the RangeExp experiment type
             challengeWidth = int(expElement.find("challenge_bits_width").text)
             step = int(expElement.find("step").text)
             numChal = math.floor((2 ** challengeWidth) / step)
             numChallengeList.append(numChal)
 
-        elif expType == "ListExp":
-            # Gestisce l'esperimento di tipo ListExp
+        elif expType == "ListExp": # Handles the ListExp experiment type
             challengesList = expElement.find("challenges_list")
             numChal = len(challengesList.findall("challenge"))
             numChallengeList.append(numChal)
 
-        elif expType == "RandomExp":
-            # Gestisce l'esperimento di tipo RandomExp
+        elif expType == "RandomExp": # Handles the RandomExp experiment type
             numChalElement = expElement.find("num_challenges")
             numChallengeList.append(int(numChalElement.text))
 
-        # Lista di ID dei PUF
+        # List of PUF IDs
         idsList = expElement.find("puf_ids")
-        ids = [int(idElement.text) for idElement in idsList.findall("id")]  # Converte in interi
-        idPufLists.append(ids)  # Aggiunge la lista di ID a idPufLists
+        ids = [int(idElement.text) for idElement in idsList.findall("id")]  # Convert each ID to an integer
+        idPufLists.append(ids)  # Add the list of PUF IDs to the main list
 
-        # Verifica e accoda i valori di respSize e countSize
+        # Add the response width to the list
         respWidth = pufInstances[ids[0]].find("respSize")
-        respWidthList.append(int(respWidth.text))  # Converte in intero e aggiunge alla lista
+        respWidthList.append(int(respWidth.text))  # Convert the response width to an integer and add it to the list
 
-        # Controllo del valore del contatore (deve essere maggiore di 0)
+        # Check if the PUF has counters
         countSize = pufInstances[ids[0]].find("countSize")
         if countSize is None or int(countSize.text) == 0:
-            hasCounters = False  # Se un contatore è 0, aggiorna il flag
+            hasCounters = False  # if there is no countSize or it is 0, then there are no counters
         else:
             hasCounters = True
         hasCountersList.append(hasCounters)
@@ -177,44 +185,48 @@ def GetTestInfo(idTest):
     return idPufLists, numChallengeList, respWidthList, numRepsList, devicesList, campaignList, hasCountersList
 
 def CalculateMetrics(idTest):
-    idPufLists,numChallengeList,ResponseWidthList,NumRepsList,devicesList,campaignList,hasCountersList=GetTestInfo(idTest)
+    """
+    Calculates various metrics for the given test ID by reading the experiment data and computing metrics like uniformity, reliability, bit aliasing, and uniqueness.
+    Args:
+        idTest (int): The ID of the test for which to calculate metrics.
+    """
+    idPufLists,numChallengeList,ResponseWidthList,NumRepsList,devicesList,campaignList,hasCountersList=_GetTestInfo(idTest)
     print(idPufLists,numChallengeList,ResponseWidthList,NumRepsList,devicesList,campaignList,hasCountersList)
     resultsCSVsBase = baseTestsDir+str(idTest)+'/results/expCampaign_'
 
     expChsLists = []
     expCRPsLists=[]
     expTemperatureList = []
-    # mapping di idDev a indexDev e viceversa
+    # map of device IDs to indices and vice versa
     idDevToIndexDev={}
     devicesValues=set()
     for idDev in devicesList:
         devicesValues.add(idDev)
     idDevToIndexDev = {val: idx for idx, val in enumerate(sorted(devicesValues))}
     indexDevToIdDev = {idx: val for idx, val in enumerate(sorted(devicesValues))}
-    # ogni ciclo è un test di tipo list, range o random su una tipologia di puf
-    # ognuno di questi test può essere stato eseguito su più istanze di puf sullo stesso dispositivo
-    #sicuramente è stato eseguito su dispositivi diversi
-    for indexExp, idPufList in enumerate(idPufLists): #idPufLists è una lista di liste di idPuf
-        
+    # each cycle is a test of type list, range or random on a type of PUF
+    # each of these tests may have been executed on multiple PUF instances on the same device
+    # it has certainly been executed on different devices
+    for indexExp, idPufList in enumerate(idPufLists): #idPufLists is a list of lists of PUF IDs for each experiment
+
         expCRPsList=[]
         expChsList = []
         
-        # la lista challenge è uguale per tutte le istanze di puf del medesimo test
+        # the challenge list is the same for all PUFs in the same experiment
         csvPath = f"{resultsCSVsBase}{campaignList[0]}.csv"
         df = pd.read_csv(csvPath)
         challengeValue = set()
         challengeValue = sorted(df[df['idpuf'].isin(idPufList)]['challenge'].unique())
 
-        # challengeValue = sorted(df[df['idpuf'] == idPufList[0]]['challenge'].unique())
         expChsList.append(challengeValue)
         challengeMap = {val: idx for idx, val in enumerate(sorted(expChsList[indexExp]))}
 
 
         print(f"devicesList: {devicesList}")
 
-        #lista di devicesList unici
+        # list of unique devicesList
         unidevicesList = list(set(devicesList))
-        #questo ciclo controlla il numero di istanze di puf su cui è stato eseguito il test
+        # this cycle checks the number of PUF instances on which the test has been executed
         for idPuf in idPufList: 
             
             crpsNpArray = np.full((numChallengeList[indexExp],ResponseWidthList[indexExp],NumRepsList[indexExp],len(unidevicesList)),np.nan) # le ultime dim sono FredDIff e Temperature
@@ -223,30 +235,29 @@ def CalculateMetrics(idTest):
             expCRPsList.append(crpsNpArray)
             print("size of crpsNpArray", crpsNpArray.shape)
             print("devicesList: ", devicesList)
-            # Itera su ogni dispositivo (identificato da idDev) per la campagna specifica
+            # cycle on each device (identified by idDev) for the specific campaign
             for index, idDev in enumerate(devicesList):
-                # Legge il CSV per la campagna specifica del dispositivo
+                # Reads the CSV file for the specific campaign of the device
                 csvPath = f"{resultsCSVsBase}{campaignList[index]}.csv"
                 df = pd.read_csv(csvPath,dtype={"response": str})
                 
-                # Filtra le righe per la PUF specifica
+                # Filter the rows for the specific PUF
                 filteredPuf = df[df['idpuf'] == idPuf]
                 
-                # Ottieni l'indice del dispositivo dalla mappatura
+                # obtain the index of the device in the crpsNpArray
                 idDevIdx = idDevToIndexDev[idDev]
                 
-                # Popola crpsNpArray con i dati dal CSV
+                # Populate crpsNpArray with data from the CSV
                 for _, row in filteredPuf.iterrows():
                     challengeVal = row['challenge']
-                    challengeIdx = challengeMap[challengeVal]  # Ottieni l'indice mappato
+                    challengeIdx = challengeMap[challengeVal]  # Obtain the index of the challenge
                 
                     responseHex = row['response']
                     temperature = row['temperature']
                     response = int(responseHex, 16)
-                    bitstringResp = f'{response:0{ResponseWidthList[indexExp]}b}'  # Conversione a binario con padding
+                    bitstringResp = f'{response:0{ResponseWidthList[indexExp]}b}'  # Convert to binary string with leading zeros
                     repIdx = int(row['numrep'])
-                    # Inserisci ogni bit della risposta nella posizione corretta
-                    #print(f"size crpsNpArray {crpsNpArray.shape}")
+                    # Insert the temperature and response bits into the respective arrays
                     TemperatueArray[challengeIdx, repIdx, idDevIdx] = temperature
                     for bitIdx, bit in enumerate(bitstringResp):
                         crpsNpArray[challengeIdx, bitIdx, repIdx, idDevIdx] = int(bit)
@@ -262,34 +273,31 @@ def CalculateMetrics(idTest):
         
         if hasCountersList[indexExp]:
             for index, idDev in enumerate(devicesList):
-                # Legge il CSV per la campagna specifica del dispositivo
+                # Read the CSV file for the specific campaign of the device
                 csvPath = f"{resultsCSVsBase}{campaignList[index]}.csv"
-                df = ExtractRelevantFields(csvPath)
+                df = _ExtractRelevantFields(csvPath)
                 MT.AnalyzeFrequencies(idTest,idDev,idPufList,df,oscillPeriod)
 
-def ExtractRelevantFields(csv_file_path):
+def _ExtractRelevantFields(csvFilePath):
     """
-    Legge un CSV, filtra i campi richiesti ed elimina le righe con counter1 o counter2 vuoti.
-
-    Parameters:
-    - csv_file_path: str, il percorso al file CSV.
-
+    Extracts relevant fields from a CSV file and returns a DataFrame with the required columns.
+    Args:
+        csvFilePath (str): The path to the CSV file.
     Returns:
-    - filtered_df: pd.DataFrame, un DataFrame contenente solo i campi richiesti e senza righe con counter1 o counter2 vuoti.
+        pd.DataFrame: A DataFrame containing the relevant fields.
     """
-    # Leggi il CSV
-    df = pd.read_csv(csv_file_path)
+    df = pd.read_csv(csvFilePath)
 
-    # Controlla se le colonne richieste esistono nel CSV
+    # Checks if the required columns are present in the DataFrame
     required_columns = ['idpuf', 'numrep', 'challenge', 'response', 'temperature', 'counter1', 'counter2']
     missing_columns = [col for col in required_columns if col not in df.columns]
     if missing_columns:
         raise ValueError(f"Le seguenti colonne sono mancanti nel CSV: {', '.join(missing_columns)}")
 
-    # Filtra solo le colonne richieste
+    # Filter only the required columns
     filtered_df = df[required_columns]
 
-    # Elimina le righe in cui counter1 o counter2 sono vuoti
+    # Delete rows where counter1 or counter2 are NaN
     filtered_df = filtered_df.dropna(subset=['counter1', 'counter2'])
 
     return filtered_df
