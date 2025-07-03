79be1c5b9540cd7815ed4ec309b6e16a5d2fe566
Daniele Lombardi <daniele.lombardi4@unina.it>
2025-07-03 10:37:22 +0200

    doc: modify HandlerPower doc

--- core/Handlers/HandlerPower.py
+++ core/Handlers/HandlerPower.py
@@ -4,49 +4,66 @@ POWER_UP = 'up'
 POWER_DOWN = 'down'
 
 def PowerUp(item,username,password):
+    """
+    Powers up a board/fan by sending a POST request to the server with the item and state.
+    Args:
+        item (str): The item to power up.
+        username (str): The username of the user.
+        password (str): The password of the user.
+    Returns:
+        bool: True if the power up request was successful, False otherwise.
+    """
     httpUrl = urljoin(GetServerAddress(), httpGetPower)
     data={'item':item,'state': POWER_UP,'username': username, 'password': password}
     response = requests.post(httpUrl,data=data, verify=certFile)
 
-    # Verifica se la richiesta è andata a buon fine (codice di stato 200)
+    # Check if the request was successful (status code 200)
     if response.status_code == 200:
         try:
-            # Ottieni la risposta JSON
+            # Get the JSON response
             data = response.json()
-            
-            # Estrai il parametro 'result' dalla risposta JSON
+            # Extract the 'result' parameter from the JSON response
             result = data.get('result')
             return result
 
         except ValueError:
-            # Gestione dell'errore nel caso la risposta non sia in formato JSON
+            # Handle the error if the response is not in JSON format
             print('[CLIENT-APP] - Powering request- Unable to parse JSON response.')
 
     else:
-        # Se la richiesta non è andata a buon fine, gestisci l'errore
+        # If the request was not successful, handle the error
         print('[CLIENT-APP] - PowerUpBoards() - Request to power up failed with error ', response.status_code)
         return False
     
 def PowerDown(item,username,password):
+    """
+    Powers down a board/fan by sending a POST request to the server with the item and state.
+    Args:
+        item (str): The item to power down.
+        username (str): The username of the user.
+        password (str): The password of the user.
+    Returns:
+        bool: True if the power down request was successful, False otherwise.
+    """
     httpUrl = urljoin(GetServerAddress(), httpGetPower)
     data={'item':item,'state': POWER_DOWN,'username': username, 'password': password}
     response = requests.post(httpUrl,data=data, verify=certFile)
 
-    # Verifica se la richiesta è andata a buon fine (codice di stato 200)
+    # Check if the request was successful (status code 200)
     if response.status_code == 200:
         try:
-            # Ottieni la risposta JSON
+            # Get the JSON response 
             data = response.json()
             
-            # Estrai il parametro 'result' dalla risposta JSON
+            # Extract the 'result' parameter from the JSON response
             result = data.get('result')
             return result
 
         except ValueError:
-            # Gestione dell'errore nel caso la risposta non sia in formato JSON
+            # Handle the error if the response is not in JSON format
             print('[CLIENT-APP] - Powering request- Unable to parse JSON response.')
 
     else:
-        # Se la richiesta non è andata a buon fine, gestisci l'errore
+        # If the request was not successful, handle the error
         print('[CLIENT-APP] - PowerDownBoardsBoards() - Request to power down failed with error ', response.status_code)
         return False
